ca72d606fc92384e351b5ad5c33b95197a7dd00a
ChanningTong <Channing_TongCN@outlook.com>
2024-10-09 14:11:09 +0800

    Initial Commit

---
+++ data_structure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data_structure
+//
+//  Created by ChanningTong on 10/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
